adb39ae44ee92199aa7e15fb027e51f05fc83767
Edaught <edaught.real@gmail.com>
2017-02-17 11:18:15 +0100

    major update fix

--- web2/view/header.php
+++ web2/view/header.php
@@ -2,22 +2,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Entreprise</title>
+	<title>Users Database</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<body>
-	<div class="container">
-		<div class="jumbotron">
-		<?php include_once("view/navbar.php");?>
-			<div class="container">
-				<h1> Welcome to my awesome page !</h1>
-				<?php include_once("view/login.php");?>
-			</div>
-		</div>
-	</div>
-</body>
-</html>
